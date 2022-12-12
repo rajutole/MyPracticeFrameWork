7feb52cc971b2c5295cdc7dfd49dfa30f2caca63
rajutole <rajutole@gmail.com>
2022-12-12 18:37:00 +0530

    dummy class

---
+++ src/test/java/awesomecucumber/utils/DummyClass.java
@@ -0,0 +1,14 @@
+package awesomecucumber.utils;
+
+public class DummyClass {
+    public static void main(String[] args) {
+        String str= "Advik";
+        int len=str.length();
+        String rev= " ";
+        for(int i=len-1;i>=0;i--){
+            rev=rev+str.charAt(i);
+
+        }
+        System.out.println("Reverse string is "+rev);
+    }
+}

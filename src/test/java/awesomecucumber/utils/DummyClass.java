49803d2f609f3154c4280df076f3d8e08c7c3514
rajutole <rajutole@gmail.com>
2022-12-21 14:05:27 +0530

    dummy print reverse string

--- src/test/java/awesomecucumber/utils/DummyClass.java
+++ src/test/java/awesomecucumber/utils/DummyClass.java
@@ -3,6 +3,7 @@ package awesomecucumber.utils;
 public class DummyClass {
     public static void main(String[] args) {
         String str= "Advik";
+        System.out.println("original String is "+str);
         int len=str.length();
         String rev= " ";
         for(int i=len-1;i>=0;i--){
